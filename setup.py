d5004b03cea38b7cf372980d15d89156805d40db
Jorge Vargas <jxv2@yahoo.com>
2023-02-13 18:27:37 -0500

    long_description value obtained from README.md file content

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import find_packages, setup
 
+# Defining a README file
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
 	name="jxvargas_fib_py",
 	version="0.0.1",
 	author="Jorge Vargas",
 	author_email="jxvargasc@gmail.com",
 	description="Calculates a Fibonacci number",
-	long_description="A basic library that \
-	calculates Fibonacci numbers",
+	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/jxvargas/jxvargas-fibonacci-py",
 	install_requires=[],
@@ -20,5 +23,3 @@ setup(
 	python_requires='>=3',
 	tests_require=['pytest'],
 )
-
-
